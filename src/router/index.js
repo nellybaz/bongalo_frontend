36838c52ddf20638540210a86a86012f55c68d27
Nelson Bassey <nellybaz10@gmail.com>
2020-02-11 10:17:18 +0200

    Create grid for images details page

--- src/router/index.js
+++ src/router/index.js
@@ -13,14 +13,11 @@ const routes = [
       title: 'Vacation Rentals, Homes, Experiences and Places - Bongalo'
     }
   },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/details',
+    name: 'details',
+    component: () => import(/* webpackChunkName: "detiails" */ '../views/ApartmentDetails.vue')
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/ApartmentDetails.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class='apartment_details'>
+        <div class="image-div">
+            <img class="img1" src="../assets/images/house4.png" alt="">
+            <img class="img2" src="../assets/images/house5.png" alt="">
+            <img class="img3" src="../assets/images/house6.png" alt="">
+            <img class="img4" src="../assets/images/house7.png" alt="">
+
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+     name:'apartment_details'
+}
+</script>
+
+
+<style lang='scss' scoped>
+    .apartment_details {
+        width:100%;
+
+        .image-div{
+            height: 500px;
+            // border:1px solid red;
+            display: grid;
+            grid-template-columns: repeat(9, 1fr);
+            grid-template-rows:1fr 1fr;
+            grid-gap: 3px;
+            img{
+                width:100%;
+                height: 100%;
+            }
+
+            .img1{
+                grid-column: 1/5;
+                grid-row: 1/-1;
+                // border:1px solid red;
+            }
+
+            .img2{
+                grid-column: 5/7;
+                grid-row: 1/2;
+                // border:1px solid red;
+            }
+
+            .img3{
+                grid-column: 5/7;
+                grid-row: 2/-1;
+                // border:1px solid red;
+            }
+
+            .img4{
+                grid-column: 7/-1;
+                grid-row: 1/-1;
+                // border:1px solid red;
+            }
+        }
+    }
+</style>
